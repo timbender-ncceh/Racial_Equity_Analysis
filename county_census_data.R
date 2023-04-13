872705c8cb2ad95195277e3dfa990b404ac44226
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-13 14:14:41 -0400

    Update county_census_data.R

--- county_census_data.R
+++ county_census_data.R
@@ -36,80 +36,80 @@ co_coc.cw$region.coc <- as.numeric(unlist(lapply(X = strsplit(co_coc.cw$`Coc/Reg
 co_coc.cw <- co_coc.cw[,c(2,5,6)]
 
 nc.counties <- full_join(all.nc.counties, 
-          co_coc.cw,
-           by = c("NAME" = "County")) %>% as_tibble()
+                         co_coc.cw,
+                         by = c("NAME" = "County")) %>% as_tibble()
 
 # cleanup
 rm(co_coc.cw, all.nc.counties)
 
 acs5_vars.cw <- rbind(data.frame(year = 2020, 
-                              variable = c("B01003_001E", # total population
-                                           "B06012_001E", # in poverty
-                                           "B02001_001E", # race - total 
-                                           "B02001_002E", # race - white alone 
-                                           "B02001_003E", # race - black or african american alone 
-                                           "B02001_004E", # race - American Indian and Alaska Native alone
-                                           "B02001_005E", # race - American Indian and Alaska Native alone 
-                                           "B02001_006E", # race - Native Hawaiian and Other Pacific Islander alone 
-                                           "B02001_007E", # race - some other race alone 
-                                           "B02001_008E", # race - two or more races
-                                           "B02001_009E", # race - two or more races, including some other race
-                                           "B02001_010E", # race - two or more races, excluding some other race, and three or more races 
-                                           "B01001_001E", # total - sex by age 
-                                           "B01001_002E", # Male - total 
-                                           "B01001_003E", # Male - 0-4   years old
-                                           "B01001_004E", # Male - 5-9   years old
-                                           "B01001_005E", # Male - 10-14 years old
-                                           "B01001_006E", # Male - 15-17 years old
-                                           "B01001_007E", # Male - 18-19 years old
-                                           "B01001_008E", # Male - 20    years old
-                                           "B01001_009E", # Male - 21    years old
-                                           "B01001_010E", # Male - 22-24  years old
-                                           "B01001_026E", # Female - total 
-                                           "B01001_027E", # Female - 0-4   years old
-                                           "B01001_028E", # Female - 5-9   years old
-                                           "B01001_029E", # Female - 10-14 years old
-                                           "B01001_030E", # Female - 15-17 years old
-                                           "B01001_031E", # Female - 18-19 years old
-                                           "B01001_032E", # Female - 20    years old
-                                           "B01001_033E", # Female - 21    years old
-                                           "B01001_034E"  # Female - 22-24  years old
-                              ), 
-                              var_def  = c("B01003_001E", # "total population",
-                                           "B06012_001E", # "in poverty",
-                                           "B02001_001E", # "race - total",
-                                           "B02001_002E", # "race - white alone",
-                                           "B02001_003E", # "race - black or african american alone",
-                                           "B02001_004E", # "race - American Indian and Alaska Native alone",
-                                           "B02001_005E", # "race - American Indian and Alaska Native alone", 
-                                           "B02001_006E", # "race - Native Hawaiian and Other Pacific Islander alone", 
-                                           "B02001_007E", # "race - some other race alone", 
-                                           "B02001_008E", # "race - two or more races",
-                                           "B02001_009E", # "race - two or more races, including some other race",
-                                           "B02001_010E", # "race - two or more races, excluding some other race, and three or more races", 
-                                           "B01001_001E", # "total - sex by age", 
-                                           "B01001_002E", # "Male - total", 
-                                           "B01001_003E", # "Male - 0-4   years old",
-                                           "B01001_004E", # "Male - 5-9   years old",
-                                           "B01001_005E", # "Male - 10-14 years old",
-                                           "B01001_006E", # "Male - 15-17 years old",
-                                           "B01001_007E", # "Male - 18-19 years old",
-                                           "B01001_008E", # "Male - 20    years old",
-                                           "B01001_009E", # "Male - 21    years old",
-                                           "B01001_010E", # "Male - 22-24  years old",
-                                           "B01001_026E", # "Female - total", 
-                                           "B01001_027E", # "Female - 0-4   years old",
-                                           "B01001_028E", # "Female - 5-9   years old",
-                                           "B01001_029E", # "Female - 10-14 years old",
-                                           "B01001_030E", # "Female - 15-17 years old",
-                                           "B01001_031E", # "Female - 18-19 years old",
-                                           "B01001_032E", # "Female - 20    years old",
-                                           "B01001_033E", # "Female - 21    years old",
-                                           "B01001_034E"  # "Female - 22-24  years old"
-                              )), 
-                   data.frame(year = 2019, 
-                              variable = c(NA), 
-                              var_def  = c(NA))) %>%
+                                 variable = c("B01003_001E", # total population
+                                              "B06012_001E", # in poverty
+                                              "B02001_001E", # race - total 
+                                              "B02001_002E", # race - white alone 
+                                              "B02001_003E", # race - black or african american alone 
+                                              "B02001_004E", # race - American Indian and Alaska Native alone
+                                              "B02001_005E", # race - American Indian and Alaska Native alone 
+                                              "B02001_006E", # race - Native Hawaiian and Other Pacific Islander alone 
+                                              "B02001_007E", # race - some other race alone 
+                                              "B02001_008E", # race - two or more races
+                                              "B02001_009E", # race - two or more races, including some other race
+                                              "B02001_010E", # race - two or more races, excluding some other race, and three or more races 
+                                              "B01001_001E", # total - sex by age 
+                                              "B01001_002E", # Male - total 
+                                              "B01001_003E", # Male - 0-4   years old
+                                              "B01001_004E", # Male - 5-9   years old
+                                              "B01001_005E", # Male - 10-14 years old
+                                              "B01001_006E", # Male - 15-17 years old
+                                              "B01001_007E", # Male - 18-19 years old
+                                              "B01001_008E", # Male - 20    years old
+                                              "B01001_009E", # Male - 21    years old
+                                              "B01001_010E", # Male - 22-24  years old
+                                              "B01001_026E", # Female - total 
+                                              "B01001_027E", # Female - 0-4   years old
+                                              "B01001_028E", # Female - 5-9   years old
+                                              "B01001_029E", # Female - 10-14 years old
+                                              "B01001_030E", # Female - 15-17 years old
+                                              "B01001_031E", # Female - 18-19 years old
+                                              "B01001_032E", # Female - 20    years old
+                                              "B01001_033E", # Female - 21    years old
+                                              "B01001_034E"  # Female - 22-24  years old
+                                 ), 
+                                 var_def  = c( "total population",
+                                               "in poverty",
+                                               "race - total",
+                                               "race - white alone",
+                                               "race - black or african american alone",
+                                               "race - American Indian and Alaska Native alone",
+                                               "race - American Indian and Alaska Native alone", 
+                                               "race - Native Hawaiian and Other Pacific Islander alone", 
+                                               "race - some other race alone", 
+                                               "race - two or more races",
+                                               "race - two or more races, including some other race",
+                                               "race - two or more races, excluding some other race, and three or more races", 
+                                               "total - sex by age", 
+                                               "Male - total", 
+                                               "Male - 0-4   years old",
+                                               "Male - 5-9   years old",
+                                               "Male - 10-14 years old",
+                                               "Male - 15-17 years old",
+                                               "Male - 18-19 years old",
+                                               "Male - 20    years old",
+                                               "Male - 21    years old",
+                                               "Male - 22-24  years old",
+                                               "Female - total", 
+                                               "Female - 0-4   years old",
+                                               "Female - 5-9   years old",
+                                               "Female - 10-14 years old",
+                                               "Female - 15-17 years old",
+                                               "Female - 18-19 years old",
+                                               "Female - 20    years old",
+                                               "Female - 21    years old",
+                                               "Female - 22-24  years old"
+                                 )), 
+                      data.frame(year = 2019, 
+                                 variable = c(NA), 
+                                 var_def  = c(NA))) %>%
   as_tibble()
 
 acs5_vars.cw
@@ -146,7 +146,7 @@ tidycensus::get_acs(geography = "county",
                                    "B01001_032E", # Female - 20    years old
                                    "B01001_033E", # Female - 21    years old
                                    "B01001_034E"  # Female - 22-24  years old
-                                   ), 
+                    ), 
                     year   = 2020, 
                     state  = "NC", 
                     survey = "acs5")
